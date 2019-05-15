346ff66cb577672073e326d055e90abb20b7e6c6
adam-brendan <adamcraveiro@gmail.com>
2019-05-15 15:15:13 +0000

    Implement renderTweets function which loops through an existing array of object tweets and adds each one to the main container of the page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,3 @@
-
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
@@ -8,57 +7,99 @@
  // Test / driver code (temporary). Eventually will get this from the server.
 
  $(document).ready(function() {
-    const tweetData = {
-        "user": {
-        "name": "Newton",
-        "avatars": {
-            "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-            "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-            "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    
+    const data = [
+        {
+          "user": {
+            "name": "Newton",
+            "avatars": {
+              "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+              "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+              "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+            },
+            "handle": "@SirIsaac"
+          },
+          "content": {
+            "text": "If I have seen further it is by standing on the shoulders of giants"
+          },
+          "created_at": 1461116232227
         },
-        "handle": "@SirIsaac"   
+        {
+          "user": {
+            "name": "Descartes",
+            "avatars": {
+              "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+              "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+              "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+            },
+            "handle": "@rd" },
+          "content": {
+            "text": "Je pense , donc je suis"
+          },
+          "created_at": 1461113959088
         },
-        "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-        "created_at": 1461116232227
-    }
+        {
+          "user": {
+            "name": "Johann von Goethe",
+            "avatars": {
+              "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+              "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+              "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+            },
+            "handle": "@johann49"
+          },
+          "content": {
+            "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+          },
+          "created_at": 1461113796368
+        }
+    ];
     
 
-    const createTweetElement = function(tweetObj) {
+    function renderTweets(tweets) {
+        tweets.forEach(function(element) {
+            let $tweetObj = createTweetElement(element)
+            $("#main-container").append($tweetObj)
+        })
+    }
+    
+      const createTweetElement = function(tweetObj) {
         
         // container
-        var $tweetBox = $("<article>").addClass("tweet rounded-corners")
+        let $tweetBox = $("<article>").addClass("tweet rounded-corners");
         
         // header
-        var $tweetHeader = $("<header>").addClass("rounded-corner-top")
-        var $tweetAvatar = $("<img>").addClass("user-avatar")
-        $tweetAvatar.attr("src", tweetData.user.avatars.regular)
-        var $tweetUsername = $("<p>").addClass("user-name header-texts")
-        $tweetUsername.text(tweetObj.user.name)
-        var $tweetUserhandle = $("<p>").addClass("user-handle header-texts")
-        $tweetUserhandle.text(tweetObj.user.handle)
+        let $tweetHeader = $("<header>").addClass("rounded-corner-top");
+        let $tweetAvatar = $("<img>").addClass("user-avatar");
+        $tweetAvatar.attr("src", tweetObj.user.avatars.regular);
+        let $tweetUsername = $("<p>").addClass("user-name header-texts");
+        $tweetUsername.text(tweetObj.user.name);
+        let $tweetUserhandle = $("<p>").addClass("user-handle header-texts");
+        $tweetUserhandle.text(tweetObj.user.handle);
         
-        $tweetHeader.append($tweetAvatar).append($tweetUsername).append($tweetUserhandle)
+        $tweetHeader.append($tweetAvatar).append($tweetUsername).append($tweetUserhandle);
 
         // body
-        var $tweetBody = $("<p>").addClass("body")
-        $tweetBody.text(tweetObj.content.text)
+        let $tweetBody = $("<p>").addClass("body");
+        $tweetBody.text(tweetObj.content.text);
 
         // footer
-        var $tweetFooter = $("<footer>").addClass("footer")
-        var $tweetPostTime = $("<p>")
-        $tweetPostTime.text(tweetData.created_at)
+        let $tweetFooter = $("<footer>").addClass("footer");
+        let $tweetPostTime = $("<p>");
+        $tweetPostTime.text(tweetObj.created_at);
+        let $tweetFlag = $("<img>").addClass("icon");
+        let $tweetRetweet = $("<img>").addClass("icon");
+        let $tweetFavorite = $("<img>").addClass("icon");
+        $tweetFlag.attr("src", "/images/flag.png");
+        $tweetRetweet.attr("src", "/images/retweet.png");
+        $tweetFavorite.attr("src", "/images/favorite.png");
         
-        $tweetFooter.append($tweetPostTime)
+        $tweetFooter.append($tweetPostTime).append($tweetFlag).append($tweetRetweet).append($tweetFavorite);
+        $tweetBox.append($tweetHeader).append($tweetBody).append($tweetFooter);
+
+        return $tweetBox;
         
-        return $tweetBox.append($tweetHeader).append($tweetBody).append($tweetFooter)
-        
-    }
+    };
     
-    var $tweet = createTweetElement(tweetData);
-    
-    // Test / driver code (temporary)
-    console.log($tweet); // to see what it looks like
-    $('#main-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
- })
+    renderTweets(data);
+ });
