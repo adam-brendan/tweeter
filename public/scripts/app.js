99fc095262d584d2448c84589600278f162ce514
adam-brendan <adamcraveiro@gmail.com>
2019-05-16 20:02:01 +0000

    Clean up code and prepare for submission

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
     
   function renderTweets(tweets) {
     tweets.forEach(function(element) {
-        let $tweetObj = createTweetElement(element)
-        $("#main-container").prepend($tweetObj)
-    })
+        let $tweetObj = createTweetElement(element);
+        $("#main-container").prepend($tweetObj);
+    });
   }
     
       const createTweetElement = function(tweetObj) {
@@ -14,14 +14,13 @@ $(document).ready(function() {
         
         // header
         let $tweetHeader = $("<header>").addClass("rounded-corner-top");
-        let $tweetAvatar = $("<img>").addClass("user-avatar");
-        $tweetAvatar.attr("src", tweetObj.user.avatars.regular);
-        let $tweetUsername = $("<p>").addClass("user-name header-texts");
-        $tweetUsername.text(tweetObj.user.name);
-        let $tweetUserhandle = $("<p>").addClass("user-handle header-texts");
-        $tweetUserhandle.text(tweetObj.user.handle);
+        let $tweetAvatar = $("<img>").addClass("user-avatar").attr("src", tweetObj.user.avatars.regular);
+        let $headerDiv = $("<div>").addClass("header-texts");
+        let $tweetUsername = $("<p>").addClass("user-name").text(tweetObj.user.name);
+        let $tweetUserhandle = $("<p>").addClass("user-handle").text(tweetObj.user.handle);
         
-        $tweetHeader.append($tweetAvatar).append($tweetUsername).append($tweetUserhandle);
+        $headerDiv.append($tweetUsername).append($tweetUserhandle);
+        $tweetHeader.append($tweetAvatar).append($headerDiv);
 
         // body
         let $tweetBody = $("<p>").addClass("body");
@@ -29,22 +28,20 @@ $(document).ready(function() {
 
         // footer
         let $tweetFooter = $("<footer>").addClass("footer");
-        let $tweetPostTime = $("<p>");
-        $tweetPostTime.text(tweetObj.created_at);
-        let $tweetFlag = $("<img>").addClass("icon");
-        let $tweetRetweet = $("<img>").addClass("icon");
-        let $tweetFavorite = $("<img>").addClass("icon");
-        $tweetFlag.attr("src", "/images/flag.png");
-        $tweetRetweet.attr("src", "/images/retweet.png");
-        $tweetFavorite.attr("src", "/images/favorite.png");
+        let $tweetPostTime = $("<p>").text(tweetObj.created_at);
+        let $tweetFlag = $("<img>").addClass("icon").attr("id", "flag").attr("src", "/images/flag.png");
+        let $tweetRetweet = $("<img>").addClass("icon").attr("id", "retweet").attr("src", "/images/retweet.png");
+        let $tweetFavorite = $("<img>").addClass("icon").attr("id", "fav").attr("src", "/images/favorite.png");
+        let $footerDiv = $("<div>").addClass("icons");
         
-        $tweetFooter.append($tweetPostTime).append($tweetFlag).append($tweetRetweet).append($tweetFavorite);
+        $footerDiv.append($tweetFlag).append($tweetRetweet).append($tweetFavorite);
+        $tweetFooter.append($tweetPostTime).append($footerDiv);
+        
         $tweetBox.append($tweetHeader).append($tweetBody).append($tweetFooter);
 
         return $tweetBox;
         
     };
-    // renderTweets(data);
     
     // handles the submit event
     $(".new-tweet form").on("submit", function(event) {
@@ -52,11 +49,11 @@ $(document).ready(function() {
       event.preventDefault();
       if ($(".tweetbox").val().length === 0) {
         $("#zero-char").addClass("displayError");
-        $(".tweetbox").addClass("errorTweetbox")
+        $(".tweetbox").addClass("errorTweetbox");
         $("#long-char").removeClass("displayError");
       } else if ($(".tweetbox").val().length > 140) {
-        $("#long-char").addClass("displayError")
-        $(".tweetbox").addClass("errorTweetbox")
+        $("#long-char").addClass("displayError");
+        $(".tweetbox").addClass("errorTweetbox");
         $("#zero-char").removeClass("displayError");
       } else {
         $("#zero-char").removeClass("displayError");
@@ -71,9 +68,9 @@ $(document).ready(function() {
         }).done(function() {
           $("#main-container").empty();
           loadTweets();
-        })
+        });
       }
-    })
+    });
 
   const loadTweets = function() {
     $.ajax({
@@ -85,9 +82,9 @@ $(document).ready(function() {
     })
     .done(function(data) {
       renderTweets(data);
-    })
-  }
+    });
+  };
 
   loadTweets();
 
- });
+});
--- public/scripts/javascript-effects.js
+++ public/scripts/javascript-effects.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
+    // character counter for new tweets
     $("textarea").keyup(function() {
         $(".counter").text(140 - ($(this).val().length));
+        $("#zero-char").removeClass("displayError");
+        $("#long-char").removeClass("displayError");
+        $(".tweetbox").removeClass("errorTweetbox");
         if ($(this).val().length > 140) {
-            $(".counter").css("color", "red")
+            $(".counter").css("color", "red");
         } else {
-            $(".counter").css("color", "#244751")
+            $(".counter").css("color", "#244751");
         }
-    })
+    });
 
+    // toggles collapse/expand of compose tweet window
     $(".compose-button").click(function() {
         $(".new-tweet").slideToggle("slow");
         $(".tweetbox").focus();
     });
-  });
+});
